3bb19dbd0f3d840bd22ee0364df9a3564519f4b4
Lucas <lucas.belderink@student.uva.nl>
2022-01-14 12:01:41 +0100

    Bord class toegevoegd en print functie voor bord begonnen

--- versie1.py
+++ versie1.py
@@ -4,8 +4,8 @@ import csv
 with open("gates&netlists/chip_0/print_0.csv") as input:
     data = [line for line in csv.reader(input)]
 
-# Skip header first line
-chips = data[1:]
+# Skip header first line for chips data, and turn chip data into ints
+chips = [list(map(int, chip)) for chip in data[1:]]
 
 max_x = 0
 max_y = 0
@@ -19,26 +19,52 @@ for coordinates in chips:
     if y > max_y:
         max_y = y
 
+class bord:
+    def __init__(self, chips, nets, width, length):
+        self.length = length
+        self.width = width
+        self.chips = chips
+        self.nets = nets
+        self.cost = 0
 
-# Create grid. Make row with 1 extra space at each end of row.
-# Add these rows two grid, with 1 extra row at eacht vertical end of grid
-grid = []
+bord1 = bord(chips, [], 8, 7)
 
-for i in range(0, max_y + 2):
-    row = []
-    for i in range(0, max_x + 2):
-        row.append(0)
-
-    grid.append(row)
-
-# Add chips to grid
-for chip in chips:
-    chip_x, chip_y = (int(chip[1]), int(chip[2]))
-    # print(chip_x, chip_y)
-
-    grid[max_y - chip_y + 1][chip_x] = int(chip[0])
+print("Bord breedte: ", bord1.width)
+print("Bord lengte: ", bord1.length)
+print("")
 
 
-for row in grid:
-    print(row)
+def print_board(board):
+    for y in range(board.length - 1, -1, -1):
+        row = []
+
+        for x in range(0, board.width):
+            al_geprint = False
+
+            for chip in board.chips:
+                
+                if (chip[1], chip[2]) == (x, y):
+                    print("", chip[0], "", end = '')
+                    al_geprint = True
+            
+            if al_geprint == False:            
+                print(" + ", end = '')
+
+        print("\n")
+
+
+print_board(bord1)
+
+        
+
+
+
+
+
+
+
+
+
+
+
 
