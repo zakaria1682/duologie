158a4649e4c9aedcb9503e0cec3cc6f2e22cb90a
Zakaria <zakaria.bensellam@live.nl>
2022-02-02 17:24:50 +0100

    add results to test_runs.py

--- test_runs.py
+++ test_runs.py
@@ -21,7 +21,6 @@ netlist_num = int(sys.argv[2])
 total_run_time = int(sys.argv[3])
 time_for_each_exec = int(sys.argv[4])
 
-
 if netlist_num < 4:
     chip_num = 0
 elif netlist_num < 7:
@@ -31,13 +30,13 @@ else:
 
 
 csvfile = open('output/output.csv', 'w')
-csvfile.write("Resultaten:")
+csvfile.write("Resultaten:" " Nets/Total nets:" " Total cost:")
 csvfile.close()
 
 n_runs = 1
 execution_times = []
 start_time = time.time()
-
+ 
 # call the algorithm while time is under a certain limit
 while time.time() - start_time < total_run_time:
     print(f"run: {n_runs}")
@@ -50,21 +49,63 @@ while time.time() - start_time < total_run_time:
                      )
     n_runs += 1
 
-
+runs = []
+cost = []
 # open csv file 
 with open('output/output.csv') as f:
     # Skip first line
     next(f)
     lines = f.readlines()
-
+    i = 1
+    j = 1
+    # check several parameters like execution times, net completions and cost
     for line in lines:
-        execution_times.append(float(line.strip()))
+        line2 = line.split(",")
+        execution_times.append(float(line2[0].strip()))
+        print("line2", line2[2])
+        cost.append(line2[2])
 
-
+        line3 = line2[1].split("/")
+        runs.append(line3[0].strip())
+        # check how many runs of the total are completed
+        if line3[0] == line3[1]:
+            i+=1 
+        j+=1
+    
 
 print("\n%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%")
 print("Average execution time: ")
 average_exec = average(execution_times)
 print(average_exec)
 print("%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%\n")
+
+
+runs = list(map(int, runs))
+runs.sort()
+execution_times = list(map(float, execution_times))
+execution_times.sort()
+cost = list(map(int, cost))
+cost.sort()
+
+
+csvfile = open('output/output.csv', 'a')
+csvfile.write("\n")
+csvfile.write("\n")
+csvfile.write(f"Algorithm has completed {n_runs} runs  ")
+csvfile.write("\n")
+csvfile.write("Average execution time: ")
+csvfile.write(str(average_exec))
+csvfile.write("\n")
+csvfile.write("total net completions ")
+csvfile.write(str(i) + ("/") + str(j) + (" ") + ("(") + str((i/j)*100) + " %)")
+csvfile.write("\n")
+csvfile.write("best net completion: ")
+csvfile.write(str(runs[0]) + ("/") + (line3[1]))
+csvfile.write("\n")
+csvfile.write("best execution time: ")
+csvfile.write(str(execution_times[0]))
+csvfile.write("\n")
+csvfile.write("best cost: ")
+csvfile.write(str(cost[0]))
+
 
