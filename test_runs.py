1b9cfbf67bc81f8e58ea660cc521c66f1104385e
Zakaria <zakaria.bensellam@live.nl>
2022-02-02 11:45:11 +0100

    improve test_runs.py

--- test_runs.py
+++ test_runs.py
@@ -4,20 +4,24 @@ import subprocess
 import os
 import pandas as pd
 
+# remove existing test csv file
+os.remove('execution_times.csv')
 
-df = pd.DataFrame(list())
-df.to_csv('execution_times.csv')
+# create empty csv file
+with open('execution_times.csv', "w") as csvfile:
+    pass
+    
 
 n_runs = 0
 execution_times = []
 start_time = time.time()
+# call the algoritme while time is under a certain limit
 while time.time() - start_time < 20:
     print(f"run: {n_runs}")
-    
-    start_time = time.time()
     subprocess.call(["timeout", "1", "python3", "BFS.py"])
     n_runs += 1
 
+# open csv file 
 with open('execution_times.csv') as f:
             new_line = f.readline().strip("\n")
             execution_times.append(float(new_line))
@@ -29,4 +33,3 @@ average_exec = average(execution_times)
 print(average_exec)
 print("%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%\n")
 
-os.remove('execution_times.csv')
