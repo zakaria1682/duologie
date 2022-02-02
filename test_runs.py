7be6fb72d96cc889f002e148e48bed848fbab4a6
Zakaria <zakaria.bensellam@live.nl>
2022-02-02 10:23:18 +0100

    add test_runs.py

--- test_runs.py
+++ test_runs.py
@@ -1,12 +1,12 @@
+from helper_functions import *
 import time
 import subprocess
 
 
-start = time.time()
 n_runs = 0
 execution_times = []
-start = time.time()
-while time.time() - start < 60:
+start_time = time.time()
+while time.time() - start_time < 60:
     print(f"run: {n_runs}")
     subprocess.call(["timeout", "1", "python3", "BFS.py"])
     n_runs += 1
@@ -14,8 +14,8 @@ while time.time() - start < 60:
     exec_time = (time.time() - start_time)
     execution_times.append(exec_time)
 
-    print("%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%")
-    print("Average execution time: ")
-    average_exec = average(execution_times)
-    print(average_exec)
-    print("%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%\n")
+print("%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%")
+print("Average execution time: ")
+average_exec = average(execution_times)
+print(average_exec)
+print("%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%\n")
